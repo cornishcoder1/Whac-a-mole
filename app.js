572c1eb4c8deda485ac91817bfea139a9dd3bbc2
cornishcoder1 <leahfisher07@hotmail.co.uk>
2023-06-19 11:22:09 +0000

    Add basic game functionality with js

--- app.js
+++ app.js
@@ -1,4 +1,55 @@
-const squares = document.querySelectorAll(".square")
-const mole = document.querySelector(".mole")
-const timeLeft = document.getElementById("time-left")
-const score = document.getElementById("score")
+const squares = document.querySelectorAll('.square')
+const mole = document.querySelector('.mole')
+const timeLeft = document.getElementById('time-left')
+const score = document.getElementById('score')
+
+let result = 0
+let hitPosition
+
+let currentTime = 20
+let timerId = null
+
+function randomSquare() {
+    squares.forEach(square => {
+        square.classList.remove('mole')
+    })
+
+    
+    let randomSquare = squares[Math.floor(Math.random() * 9)]
+    // console.log(randomPosition)
+    // console.log(Math.floor(Math.random() * 9))
+    randomSquare.classList.add('mole')
+
+    hitPosition = randomSquare.id
+}
+
+squares.forEach(square => {
+    square.addEventListener('mousedown', () => {
+        if (square.id == hitPosition) {
+            result++
+            score.textContent = result
+            hitPosition = null
+        }
+    })    
+})
+
+function moveMole() {
+    timerId = setInterval(randomSquare, 500)
+}
+
+//TASK Attach to a button
+moveMole()
+
+function countDown() {
+    currentTime--
+    timeLeft.textContent = currentTime
+   
+    if (currentTime == 0) {
+      clearInterval(countDownTimerId)
+      clearInterval(timerId)
+      alert('GAME OVER! Your final score is ' + result)
+    }
+   
+   }
+
+let countDownTimerId = setInterval(countDown, 1000)
