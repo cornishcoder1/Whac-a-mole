7fe2783e6d6378d731a09c9c1839f2b34e2a1240
cornishcoder1 <leahfisher07@hotmail.co.uk>
2023-06-19 10:30:39 +0000

    Set variables in app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+const squares = document.querySelectorAll(".square")
+const mole = document.querySelector(".mole")
+const timeLeft = document.getElementById("time-left")
+const score = document.getElementById("score")
